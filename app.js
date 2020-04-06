3e7a727e89e84d930b60451e6f478d38c295dc25
vincentayorinde <vincentayorinde@yahoo.com>
2020-04-06 20:10:23 +0100

    add cors

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const userRoutes = require('./api/routes/users');
 
-// app.use('*', (res, req, next) => {
-//     res.status(404).json({
-//         message: 'Route does not exist'
-//     });
-// });
-app.use('/users', userRoutes)
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// Routes which should handle requests
+app.use('/users', userRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 module.exports = app;
