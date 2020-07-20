93f2ec507c1079cdc713ac63fe805b835e8169d6
Vincent Olagbemide <vincentayorinde@yahoo.com>
2020-07-20 15:09:23 +0100

    update welcome messge

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ Routes(app);
 
 app.get('/', (req, res) =>
     res.status(200).json({
-        message: 'Welcome to uVote API',
+        message: 'Welcome to Touch API',
     })
 );
 app.use((req, res) =>
